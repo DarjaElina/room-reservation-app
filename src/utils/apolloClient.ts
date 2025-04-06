c56ae265be44be6d054c5624dfa5bdc773c8a6f7
DarjaElina <daria.elina98@gmail.com>
2025-04-06 23:16:31 +0300

    update packages

--- src/utils/apolloClient.ts
+++ src/utils/apolloClient.ts
@@ -7,9 +7,7 @@ import { Reference } from '@apollo/client/utilities';
 
 const authStorage = new AuthStorage();
 
-const BACKEND_URL = __DEV__
-  ? process.env.EXPO_PUBLIC_BACKEND_URL
-  : process.env.EXPO_PUBLIC_BACKEND_URL_PROD;
+const BACKEND_URL = process.env.EXPO_PUBLIC_BACKEND_URL;
 
 const httpLink = createHttpLink({
   uri: BACKEND_URL,
