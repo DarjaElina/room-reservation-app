8809edce4e40f16addd0d75982ce1668b96d146b
DarjaElina <daria.elina98@gmail.com>
2025-04-23 22:38:54 +0300

    update backend url

--- src/utils/apolloLinks.ts
+++ src/utils/apolloLinks.ts
@@ -6,8 +6,7 @@ import { onError } from '@apollo/client/link/error';
 const authStorage = new AuthStorage();
 
 export const httpLink = createHttpLink({
-  //uri: process.env.EXPO_PUBLIC_BACKEND_URL,
-  uri: 'http://localhost:4000/',
+  uri: process.env.EXPO_PUBLIC_BACKEND_URL,
 });
 
 export const authLink = setContext(async (_, previousContext) => {
