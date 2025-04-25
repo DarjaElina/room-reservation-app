bae476e551f3fcbfcc425a284dfb448d4b76966c
DarjaElina <daria.elina98@gmail.com>
2025-04-25 12:33:29 +0300

    use refetchQueries instead of cache update for allRooms

--- src/hooks/useFavoriteRoom.ts
+++ src/hooks/useFavoriteRoom.ts
@@ -4,6 +4,7 @@ import { FIND_ROOM, ALL_ROOMS } from '../graphql/queries';
 
 const useFavoriteRoom = ({ roomId }: { roomId: string }) => {
   const [mutate, { loading }] = useMutation(TOGGLE_FAVORITE, {
+    refetchQueries: [ALL_ROOMS],
     update: (cache, { data: toggleData }) => {
       if (!toggleData?.toggleFavorite.id) return;
       cache.updateQuery(
@@ -24,52 +25,6 @@ const useFavoriteRoom = ({ roomId }: { roomId: string }) => {
           };
         }
       );
-
-      const roomData = cache.readQuery({
-        query: ALL_ROOMS,
-      });
-
-      if (!roomData) return;
-
-      const roomToAddOrDelete = roomData.rooms.edges.find(
-        (r) => r.node.id === toggleData.toggleFavorite.id
-      );
-
-      if (roomToAddOrDelete) {
-        if (toggleData.toggleFavorite.isFavoriteNow) {
-          cache.writeQuery({
-            query: ALL_ROOMS,
-            variables: {
-              showFavorites: true,
-            },
-            data: {
-              rooms: {
-                ...roomData.rooms,
-                edges: roomData.rooms.edges.concat(roomToAddOrDelete),
-                pageInfo: roomData.rooms.pageInfo,
-              },
-            },
-            broadcast: false,
-          });
-        } else {
-          cache.writeQuery({
-            query: ALL_ROOMS,
-            variables: {
-              showFavorites: true,
-            },
-            data: {
-              rooms: {
-                ...roomData.rooms,
-                edges: roomData.rooms.edges.filter(
-                  (r) => r.node.id !== roomToAddOrDelete.node.id
-                ),
-                pageInfo: roomData.rooms.pageInfo,
-              },
-            },
-            broadcast: false,
-          });
-        }
-      }
     },
   });
 
