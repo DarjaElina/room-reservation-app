803c400db45ef4f8054e778867e25c38d09a4d4d
DarjaElina <daria.elina98@gmail.com>
2025-04-26 23:53:49 +0300

    use apollo's toReference in cache modifying function

--- src/hooks/useBooking.ts
+++ src/hooks/useBooking.ts
@@ -1,7 +1,5 @@
 import { useMutation } from '@apollo/client';
 import { CREATE_BOOKING } from '@/src/graphql/mutations';
-import { BOOKINGS } from '../graphql/queries';
-import { BookingsQuery } from '@/__generated__/graphql';
 
 const useBooking = (): [
   (
@@ -14,15 +12,13 @@ const useBooking = (): [
   const [mutate, { loading }] = useMutation(CREATE_BOOKING, {
     update: (cache, { data }) => {
       if (!data?.createBooking) return;
-      const bookingsData = cache.readQuery({ query: BOOKINGS }) || {
-        bookings: [],
-      };
-      cache.writeQuery({
-        query: BOOKINGS,
-        data: {
-          ...data,
-          bookings: bookingsData?.bookings.concat(data?.createBooking),
-        } as BookingsQuery,
+
+      cache.modify({
+        fields: {
+          bookings(existingBookings = [], { toReference }) {
+            return [...existingBookings, toReference(data.createBooking)];
+          },
+        },
         broadcast: false,
       });
     },
